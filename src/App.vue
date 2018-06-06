ae0937baaa31e2fb70078c08a70ac55ecaf51b6c
Aaron Oxenrider <theox26@gmail.com>
2018-06-06 16:48:02 -0400

    style and fa elements

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,63 @@
 <template>
   <div id="app">
+    <el-container>
+      <el-aside width="3rem" class="main-left-bar">
+        <font-awesome-layers>
+          <font-awesome-icon icon="circle" transform="shrink-7" :mask="['fas', 'circle']" />
+        </font-awesome-layers>
+        <font-awesome-layers>
+          <font-awesome-icon :icon="['far', 'circle']" transform="grow-8" style="color: #FEC010;" />
+          <font-awesome-icon icon="circle" transform="shrink-7" :mask="['fas', 'circle']" />
+        </font-awesome-layers>
+      </el-aside>
+      <el-aside width="200px">
+        <el-menu
+          default-active="2"
+          class="el-menu-vertical-demo"
+          @open="handleOpen"
+          @close="handleClose"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b">
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>Navigator One</span>
+            </template>
+            <el-menu-item-group title="Group One">
+              <el-menu-item index="1-1">item one</el-menu-item>
+              <el-menu-item index="1-2">item one</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="Group Two">
+              <el-menu-item index="1-3">item three</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="1-4">
+              <template slot="title">item four</template>
+              <el-menu-item index="1-4-1">item one</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-menu-item index="2">
+            <i class="el-icon-menu"></i>
+            <span>Navigator Two</span>
+          </el-menu-item>
+          <el-menu-item index="3" disabled>
+            <i class="el-icon-document"></i>
+            <span>Navigator Three</span>
+          </el-menu-item>
+          <el-menu-item index="4">
+            <i class="el-icon-setting"></i>
+            <span>Navigator Four</span>
+          </el-menu-item>
+        </el-menu>
+      </el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>
           <router-view/>
+        </el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
@@ -13,10 +70,20 @@ export default {
     FontAwesomeIcon,
     FontAwesomeLayers,
   },
+  methods: {
+    handleOpen(key, keyPath) {
+      // eslint-disable-next-line
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      // eslint-disable-next-line
+      console.log(key, keyPath);
+    },
+  },
 };
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -30,5 +97,17 @@ export default {
 body {
   margin: 0;
   padding: 0;
+}
+
+.main-left-bar {
+  background: #ccc;
+  color: #fff;
+  padding-top: 3rem;
+  width: 3rem;
+
+  .fa-layers {
+    margin: 1.5rem auto;
+    display: block;
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,18 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import Element from 'element-ui';
+import ElementUI from 'element-ui';
 import locale from 'element-ui/lib/locale/lang/en';
 import fontawesome from '@fortawesome/fontawesome';
 import solid from '@fortawesome/fontawesome-pro-solid';
+import regular from '@fortawesome/fontawesome-pro-regular';
 import 'element-ui/lib/theme-chalk/index.css';
 import './hr-dashboard.scss';
 // eslint-disable-next-line
 import App from './App';
 import router from './router';
 
-Vue.use(Element, { locale });
-fontawesome.library.add(solid);
+Vue.use(ElementUI, { locale });
+fontawesome.library.add(solid, regular);
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
