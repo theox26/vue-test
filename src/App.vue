a6a1fb09ac0852f4ec2f1d88ab20c5ac6f562ddc
Aaron Oxenrider <theox26@gmail.com>
2018-06-07 17:04:49 -0400

    components

--- src/App.vue
+++ src/App.vue
@@ -2,56 +2,73 @@
   <div id="app">
     <el-container>
       <el-aside width="3rem" class="main-left-bar">
-        <font-awesome-layers>
-          <font-awesome-icon icon="circle" transform="shrink-7" :mask="['fas', 'circle']" />
-        </font-awesome-layers>
-        <font-awesome-layers>
-          <font-awesome-icon :icon="['far', 'circle']" transform="grow-8" style="color: #FEC010;" />
-          <font-awesome-icon icon="circle" transform="shrink-7" :mask="['fas', 'circle']" />
-        </font-awesome-layers>
+        <div class="hamburger-box" @click="viewMenu = !viewMenu">
+          <font-awesome-icon icon="bars" size="lg" />
+        </div>
+        <left-nav-button />
+        <left-nav-button selected="true" />
       </el-aside>
-      <el-aside width="200px">
-        <el-menu
-          default-active="2"
-          class="el-menu-vertical-demo"
-          @open="handleOpen"
-          @close="handleClose"
-          background-color="#545c64"
-          text-color="#fff"
-          active-text-color="#ffd04b">
-          <el-submenu index="1">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>Navigator One</span>
-            </template>
-            <el-menu-item-group title="Group One">
-              <el-menu-item index="1-1">item one</el-menu-item>
-              <el-menu-item index="1-2">item one</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="Group Two">
-              <el-menu-item index="1-3">item three</el-menu-item>
-            </el-menu-item-group>
-            <el-submenu index="1-4">
-              <template slot="title">item four</template>
-              <el-menu-item index="1-4-1">item one</el-menu-item>
+      <transition name="fade" mode="out-in" appear>
+        <el-aside width="200px" v-show="viewMenu" class="nav-menu-vertical">
+          <el-menu
+            default-active="2"
+            class="nav-menu-vertical"
+            @open="handleOpen"
+            @close="handleClose"
+            background-color="#383d42"
+            text-color="#fff"
+            active-text-color="#ffd04b">
+            <el-submenu index="1">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>Navigator One</span>
+              </template>
+              <el-menu-item-group title="Group One">
+                <el-menu-item index="1-1">item one</el-menu-item>
+                <el-menu-item index="1-2">item one</el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="Group Two">
+                <el-menu-item index="1-3">item three</el-menu-item>
+              </el-menu-item-group>
+              <el-submenu index="1-4">
+                <template slot="title">item four</template>
+                <el-menu-item index="1-4-1">item one</el-menu-item>
+              </el-submenu>
             </el-submenu>
-          </el-submenu>
-          <el-menu-item index="2">
-            <i class="el-icon-menu"></i>
-            <span>Navigator Two</span>
-          </el-menu-item>
-          <el-menu-item index="3" disabled>
-            <i class="el-icon-document"></i>
-            <span>Navigator Three</span>
-          </el-menu-item>
-          <el-menu-item index="4">
-            <i class="el-icon-setting"></i>
-            <span>Navigator Four</span>
-          </el-menu-item>
-        </el-menu>
-      </el-aside>
+            <el-menu-item index="2">
+              <i class="el-icon-menu"></i>
+              <span>Navigator Two</span>
+            </el-menu-item>
+            <el-menu-item index="3" disabled>
+              <i class="el-icon-document"></i>
+              <span>Navigator Three</span>
+            </el-menu-item>
+            <el-menu-item index="4">
+              <i class="el-icon-setting"></i>
+              <span>Navigator Four</span>
+            </el-menu-item>
+          </el-menu>
+        </el-aside>
+      </transition>
       <el-container>
-        <el-header>Header</el-header>
+        <el-header
+          class="page-header"
+          height="3rem"
+        >
+          <div class="header-left header">
+            <div>
+              Page Title
+            </div>
+          </div>
+          <div class="header-right header">
+            <div>
+              Hello, FirstName LastName
+              <span>
+                <font-awesome-icon icon="caret-down" />
+              </span>
+            </div>
+          </div>
+        </el-header>
         <el-main>
           <router-view/>
         </el-main>
@@ -63,12 +80,19 @@
 
 <script>
 import { FontAwesomeIcon, FontAwesomeLayers } from '@fortawesome/vue-fontawesome';
+import LeftNavButton from './components/LeftNavButton.vue';
 
 export default {
   name: 'App',
+  data() {
+    return {
+      viewMenu: false,
+    };
+  },
   components: {
     FontAwesomeIcon,
     FontAwesomeLayers,
+    LeftNavButton,
   },
   methods: {
     handleOpen(key, keyPath) {
@@ -99,14 +123,55 @@ body {
   padding: 0;
 }
 
+aside {
+  min-height: 100vh;
+}
+
+.hamburger-box {
+  height: 3rem;
+  width: 100%;
+  background-color: #545c64;
+  line-height: 3rem;
+  margin-bottom: 1.5rem;
+}
+
+.nav-menu-vertical {
+  background-color: #383d42;
+  border-right: none;
+}
+
+.page-header {
+  box-shadow: 0 2px 5px #ccc;
+  position: relative;
+  display: flex;
+
+  .header {
+    width: 50%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    &.header-left > div {
+      display: flex;
+      flex-direction: row;
+      justify-content: flex-start;
+    }
+
+    &.header-right > div {
+      display: flex;
+      flex-direction: row;
+      justify-content: flex-end;
+    }
+  }
+}
+
 .main-left-bar {
   background: #ccc;
   color: #fff;
-  padding-top: 3rem;
   width: 3rem;
 
-  .fa-layers {
-    margin: 1.5rem auto;
+  .left-nav-button {
+    margin: 0 auto 2rem auto;
     display: block;
   }
 }
---
+++ src/components/LeftNavButton.vue
@@ -0,0 +1,36 @@
+<template>
+  <font-awesome-layers class="left-nav-button">
+    <font-awesome-icon
+      :icon="['far', 'circle']"
+      transform="grow-8"
+      style="color: #FEC010;"
+      v-show="isSelected"
+    />
+    <font-awesome-icon icon="circle" transform="shrink-7" :mask="['fas', 'circle']" />
+  </font-awesome-layers>
+</template>
+
+<script>
+import { FontAwesomeIcon, FontAwesomeLayers } from '@fortawesome/vue-fontawesome';
+
+export default {
+  name: 'LeftNavButton',
+  components: {
+    FontAwesomeIcon,
+    FontAwesomeLayers,
+  },
+  props: {
+    selected: Boolean,
+  },
+  computed: {
+    isSelected() {
+      return this.selected;
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+
+</style>
